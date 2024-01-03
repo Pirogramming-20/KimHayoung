289086d061b54bc39b5fe1839976d14d03baa414
HayoungGloria <glory1251@naver.com>
2024-01-04 03:59:58 +0900

    쇼핑몰 미니게임 & 코드리뷰 JavaScript-display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -5,13 +5,18 @@ function loadItems(){
     .then(json => json.items);
 }
 
-functiondisplayItems(items) {
+//update the list with the given items
+function displayItems(items) {
     const container = document.querySelector('.items');
     container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
+//create html list item from the given data item 
 function createHTMLString(item) {
     return `
-
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item_thumbnail" />
+        <span class="item_description">${item.gender}, ${item.size}</span>
+    </li>
     `;
 }
 // main
