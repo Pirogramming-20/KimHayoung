e8f8b6277cdb29159d6128789c54d232a474db79
HayoungGloria <glory1251@naver.com>
2024-01-06 04:22:48 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,36 +1,38 @@
-num=0
 
-count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+def brGame(player, num): 
 
-while not count.isdigit() or int(count) not in [1, 2, 3]:
-    if not count.isdigit():
-        print("정수를 입력하세요")
-    else:
-        print("1, 2, 3 중 하나를 입력하세요")
     count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-count = int(count)
+
+    while not count.isdigit() or int(count) not in [1, 2, 3]:
+        if not count.isdigit():
+            print("정수를 입력하세요")
+        else:
+            print("1, 2, 3 중 하나를 입력하세요")
+        count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+    count = int(count)
 
 
 
-for i in range(count):
-    num += 1
-    print(f"playerA: {num}")
+    for i in range(count):
+        num += 1
+        print(f"{player} : {num}")
+        if num >= 31:
+            return num
+    return num
+
+def playBR31():
+    num = 0
+    while num < 31:
+        num = brGame("playerA", num)
+        if num >= 31:
+            print("playerB win!")
+            break
+        num = brGame("playerB", num)
+        if num >= 31:
+            print("playerA win!")
+            break
 
 
-count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-
-while not count.isdigit() or int(count) not in [1, 2, 3]:
-    if not count.isdigit():
-        print("정수를 입력하세요")
-    else:
-        print("1, 2, 3 중 하나를 입력하세요")
-    count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-count = int(count)
-
-
-
-for i in range(count):
-    num += 1
-    print(f"playerB: {num}")
+playBR31()
     
         
